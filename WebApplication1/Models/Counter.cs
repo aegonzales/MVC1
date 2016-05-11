b241eda3b8919d78bdc32650d16c6c2262ee35b3
aegonzales <aeg.gonzales@hotmail.ph>
2016-05-12 00:10:48 +0800

    MVC1

---
+++ WebApplication1/Models/Counter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Counter
+    {
+        [Key]
+        public int CounterId { get; set; }
+        public int CounterCount { get; set; }
+    }
+}
---
+++ WebApplication1/Models/CounterBusinessLayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.DataAccessLayer;
+
+namespace WebApplication1.Models
+{
+    public class CounterBusinessLayer
+    {
+        public int GetMax()
+        {
+            CounterDAL test = new CounterDAL();
+            return test.Counters.Max(x => x.CounterCount);
+        }
+
+        public Counter SaveCounter(Counter c)
+        {
+            CounterDAL test = new CounterDAL();
+            test.Counters.Add(c);
+            test.SaveChanges();
+            return c;
+        }
+
+        public bool Delete()
+        {
+            try
+            {
+                CounterDAL test = new CounterDAL();
+                test.Database.ExecuteSqlCommand("TRUNCATE TABLE TblCounter");
+                DefaultValue();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private bool DefaultValue()
+        {
+            Counter defData = new Counter();
+            defData.CounterCount = 1;
+            SaveCounter(defData);
+            return true;
+        }
+    }
+}
